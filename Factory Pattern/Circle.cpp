e6f0838b6c122b3f29a7a97448e829135651ba9f
Hedax01 <nqhuy2205@gmail.com>
2017-07-18 10:51:23 +0900

    Circle object

---
+++ Factory Pattern/Circle.cpp
@@ -0,0 +1,17 @@
+#include "Circle.h"
+
+
+
+Circle::Circle()
+{
+}
+
+void Circle::draw()
+{
+	std::cout << "Inside Circle:: draw() method\n";
+}
+
+
+Circle::~Circle()
+{
+}
---
+++ Factory Pattern/Circle.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Shape.h"
+class Circle :
+	public Shape
+{
+public:
+	Circle();
+	void draw();
+	~Circle();
+};
+
